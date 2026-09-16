57dafa2dad0506ac154929f6b360f74e681aecc8
tawfiqtax43 <tawfiqtax43@gmail.com>
2026-09-16 14:13:40 +0600

    Update app.py
    create driver changed

--- app.py
+++ app.py
@@ -21,9 +21,12 @@ def create_driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120.0.0.0 Safari/537.36")
     
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    # Chromium লোকেশন সেট করা
+    chrome_options.binary_location = "/usr/bin/chromium"
+    
+    service = Service("/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
 
 def verify_single(driver, clean_chl):
